5408df1d03c59b6f59e79dbaaff4e1feaf586796
Nguyen0110 <120012317+Nguyen0110@users.noreply.github.com>
2023-11-04 16:24:32 +0700

    P0079

---
+++ P0079/src/main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import controller.FileManager;
+/**
+ *
+ * @author USER
+ */
+public class main {
+    public static void main(String[] args) {
+        new FileManager().run();
+    }
+}
